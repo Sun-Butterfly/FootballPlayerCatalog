9747ae3ce698b068e6811059a602fe6de550ebdb
Leroy <sunbutterfly979@gmail.com>
2024-09-17 16:52:47 +0300

    доделана кнопка редактирования, кнопка функциональна

--- FootballPlayersCatalogFrontend/src/app/all-football-players/all-football-players.component.ts
+++ FootballPlayersCatalogFrontend/src/app/all-football-players/all-football-players.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FootballPlayer, HttpService, Team} from "../http.service";
 import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-all-football-players',
@@ -19,16 +19,18 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 })
 export class AllFootballPlayersComponent implements OnInit {
   title = 'Список футболистов';
-  subtitle ='Нажмите на футболиста, чтобы узнать больше информации';
+  subtitle = 'Нажмите на футболиста, чтобы узнать больше информации';
   playerFormGroup: FormGroup;
   footballPlayers: FootballPlayer[] = [];
   countries: string[] = [];
-  genders: string[] =[];
-  teams:Team[]=[];
+  genders: string[] = [];
+  teams: Team[] = [];
   activePlayer: number = -1;
   editMode: boolean = false;
+
   constructor(private http: HttpService) {
     this.playerFormGroup = new FormGroup({
+      id: new FormControl(null, [Validators.required]),
       name: new FormControl(null, [Validators.required]),
       lastName: new FormControl(null, [Validators.required]),
       gender: new FormControl(null, [Validators.required]),
@@ -40,13 +42,13 @@ export class AllFootballPlayersComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.http.getAllCountries().subscribe(countries=>{
+    this.http.getAllCountries().subscribe(countries => {
       this.countries = countries;
     })
-    this.http.getAllGenders().subscribe(genders=>{
+    this.http.getAllGenders().subscribe(genders => {
       this.genders = genders;
     })
-    this.http.getAllTeams().subscribe(teams=>{
+    this.http.getAllTeams().subscribe(teams => {
       this.teams = teams;
     })
     this.http.getAllFootballPlayers().subscribe(players => {
@@ -56,37 +58,51 @@ export class AllFootballPlayersComponent implements OnInit {
 
   setActivePlayer(i: number) {
     if (this.activePlayer === i) {
-      this.activePlayer = -1;
+      this.resetAll();
     } else {
       this.activePlayer = i;
     }
   }
 
+  resetAll(){
+    this.activePlayer = -1;
+    this.editMode=false;
+  }
+
   deleteFootballPlayer($event: MouseEvent, i: number) {
     $event.stopPropagation();
     let id = this.footballPlayers[i].id;
-    this.http.deleteFootballPlayer(id).subscribe(()=>{
-      this.footballPlayers = this.footballPlayers.filter(x => x.id !== id)
+    this.http.deleteFootballPlayer(id).subscribe(() => {
+      this.footballPlayers = this.footballPlayers.filter(x => x.id !== id);
     })
   }
 
   updateFootballPlayer($event: MouseEvent, i: number) {
     $event.stopPropagation();
-    let footballPlayer = {...this.footballPlayers[i],...this.playerFormGroup.value};
+    let footballPlayer = {...this.footballPlayers[i], ...this.playerFormGroup.value};
     this.http.updateFootballPlayer(footballPlayer).subscribe(() => {
+      this.http.getAllFootballPlayers().subscribe(players => {
+        this.footballPlayers = players;
+      })
       alert('Успешно отправлено')
     })
+    this.changeEditMode($event, i);
+    this.activePlayer = -1;
   }
 
-  changeEditMode(i : number) {
-    if(!this.editMode){
+  changeEditMode($event: MouseEvent, i: number) {
+    if (!this.editMode) {
       this.editMode = true;
-      console.log(this.footballPlayers, this.activePlayer)
-      this.playerFormGroup.patchValue(this.footballPlayers[i])
-    }
-    else{
-      this.editMode = false;
+      this.playerFormGroup.patchValue({
+        ...this.footballPlayers[i],
+        dateOfBirth: formatDate(new Date(this.footballPlayers[i].dateOfBirth), 'yyyy-MM-dd', 'en')
+      })
+      this.activePlayer=-1;
+    } else {
+      this.resetAll();
     }
   }
+
+
 }
 
